401fa66d01fba255cb872867721c4175390e9d39
longzhiyou <long.zhiyou@bestskip.com>
2017-06-20 18:07:10 +0800

    优化

--- server/src/main/java/com/lzy/rule/WuXingJingJiRule.groovy
+++ server/src/main/java/com/lzy/rule/WuXingJingJiRule.groovy
@@ -16,6 +16,7 @@ public class WuXingJingJiRule extends AnalyzeResult implements BaseRule{
         def mapRule = [:]
         def matchStr = ""
         def ruleValue ;
+        def tempShow=""
 
         def  nianGan = bazi.getNianGan()
         def nianZhi = bazi.getNianZhi()
@@ -39,6 +40,17 @@ public class WuXingJingJiRule extends AnalyzeResult implements BaseRule{
 
         def luZhi = commonAlgorithm.getLuZhi(nianGan);
 
+        //五虎遁禄干
+        def mapDunLuGan=["甲":"丙","乙":"己","丙":"癸","丁":"丙"
+                    ,"戊":"丁","己":"庚",
+                    "庚":"甲","辛":"丁","壬":"辛","癸":"甲"]
+        //天乙贵人
+        def mapTianYi=["甲":"丑未" ,"戊":"丑未" ,"庚":"丑未",
+                       "乙":"子申" ,"己":"子申" ,
+                       "丙":"亥酉" ,"丁":"亥酉" ,
+                       "壬":"巳卯" ,"癸":"巳卯" ,
+                       "辛":"午寅"]
+
 
         //荣神格
         matchStr = yueZhi+riGan+shiGan
@@ -60,11 +72,11 @@ public class WuXingJingJiRule extends AnalyzeResult implements BaseRule{
         def mingweilu = ["甲丙寅","乙丁卯","庚壬申","辛癸酉"]
         matchStr = nianGan+shiZhu
         if (mingweilu.contains(matchStr)) {
-            mapResult["时柱【名位禄】"]="凡禄上带食神者是，如甲人见丙寅，谓之名位禄，皆为贵格，余仿此。（林开五命）"
+            mapResult["【名位禄】时柱"]="凡禄上带食神者是，如甲人见丙寅，谓之名位禄，皆为贵格，余仿此。（林开五命）"
         }
         matchStr = nianGan+riZhu
         if (mingweilu.contains(matchStr)) {
-            mapResult["日柱【名位禄】"]="凡禄上带食神者是，如甲人见丙寅，谓之名位禄，皆为贵格，余仿此。（林开五命）"
+            mapResult["【名位禄】日柱"]="凡禄上带食神者是，如甲人见丙寅，谓之名位禄，皆为贵格，余仿此。（林开五命）"
         }
 
         //【生成禄】
@@ -72,11 +84,11 @@ public class WuXingJingJiRule extends AnalyzeResult implements BaseRule{
                             "庚庚申","辛辛酉","壬癸亥","癸壬子"]
         matchStr = nianGan+shiZhu
         if (shengchenglu.contains(matchStr)) {
-            mapResult["时柱【生成禄】"]=matchStr
+            mapResult["【生成禄】时柱"]=matchStr
         }
         matchStr = nianGan+riZhu
         if (shengchenglu.contains(matchStr)) {
-            mapResult["日柱【生成禄】"]=matchStr
+            mapResult["【生成禄】日柱"]=matchStr
         }
 
         //【真禄】五虎遁得到禄位天干,又得到此天干的禄位
@@ -94,10 +106,10 @@ public class WuXingJingJiRule extends AnalyzeResult implements BaseRule{
                     "庚":"甲申","辛":"乙酉","壬":"丁亥","癸":"丙子"]
         ruleValue= mapRule.get(nianGan);
         if (shiZhu==ruleValue) {
-            mapResult["时柱【禄头财】"]="主人富有声望。古诗云：禄生于絪缛，因财反有名，君子荣显禄，常人主丰盈。"
+            mapResult["【禄头财】时柱"]="主人富有声望。古诗云：禄生于絪缛，因财反有名，君子荣显禄，常人主丰盈。"
         }
         if (riZhu==ruleValue) {
-            mapResult["日柱【禄头财】"]="主人富有声望。古诗云：禄生于絪缛，因财反有名，君子荣显禄，常人主丰盈。"
+            mapResult["【禄头财】日柱"]="主人富有声望。古诗云：禄生于絪缛，因财反有名，君子荣显禄，常人主丰盈。"
         }
 
 
@@ -107,10 +119,10 @@ public class WuXingJingJiRule extends AnalyzeResult implements BaseRule{
                  "庚":"丙申","辛":"丁酉","壬":"己亥","癸":"戊子"]
         ruleValue= mapRule.get(nianGan)
         if (shiZhu==ruleValue) {
-            mapResult["时柱【禄头鬼】"]="又名赤舌杀，君子主甲科，常人口舌刑责，若别有贵救，主家世超越，三代富有，行年太岁遇之，主有灾。（广信录）"
+            mapResult["【禄头鬼】时柱"]="又名赤舌杀，君子主甲科，常人口舌刑责，若别有贵救，主家世超越，三代富有，行年太岁遇之，主有灾。（广信录）"
         }
         if (riZhu==ruleValue) {
-            mapResult["日柱【禄头鬼】"]="又名赤舌杀，君子主甲科，常人口舌刑责，若别有贵救，主家世超越，三代富有，行年太岁遇之，主有灾。（广信录）"
+            mapResult["【禄头鬼】日柱"]="又名赤舌杀，君子主甲科，常人口舌刑责，若别有贵救，主家世超越，三代富有，行年太岁遇之，主有灾。（广信录）"
         }
 
         //【禄盈天府格】
@@ -133,7 +145,49 @@ public class WuXingJingJiRule extends AnalyzeResult implements BaseRule{
             }
         }
 
+        //【暗禄格】五虎遁禄双六合
+        mapRule=["甲":"辛亥","乙":"甲戌","丙":"戊申","丁":"辛未"
+                 ,"戊":"壬申","己":"乙未",
+                 "庚":"己巳","辛":"壬辰","壬":"丙寅","癸":"己丑"]
+        ruleValue= mapRule.get(nianGan)
+        tempShow ="此是上清暗合禄，不因师指若何知。"+ nianGan+"人"+ruleValue
+        if (shiZhu==ruleValue) {
+            mapResult["【暗禄格】时柱"]=tempShow
+        }
+        if (riZhu==ruleValue) {
+            mapResult["【暗禄格】日柱"]=tempShow
+        }
 
+        //【天雄禄】甲人数至戊是五数，不必求戊，戊禄在巳，见巳即名天雄禄，更正禄加之为入格。（马子才命格）
+        mapRule=["甲":"巳","乙":"午","丙":"申","丁":"酉"
+                 ,"戊":"亥","己":"子",
+                 "庚":"寅","辛":"卯","壬":"巳","癸":"午"]
+        ruleValue= mapRule.get(nianGan)
+        if (listMingZhi.contains(ruleValue)&& listMingZhi.contains(luZhi)) {
+            mapResult["【天雄禄】"]="甲人数至戊是五数，不必求戊，戊禄在巳，见巳即名天雄禄，更正禄加之为入格。（马子才命格）"
+        }
+
+        //【天禄贵神】太岁本干不见禄，取五虎元遁，至禄上看得何干，真干见天乙贵神，而贵人所坐之干，复得贵于禄上者是也
+        def dunLuShangGan = mapDunLuGan.get(nianGan)
+        if (listMingGan.contains(dunLuShangGan)) {
+
+            def tianyi = mapTianYi.get(dunLuShangGan)
+            for(i in 0..1){
+                if (listMingZhi.contains(tianyi[i])) {
+                    for(j in 0..3){
+                        if (tianyi[i]==listMingZhi[j]) {
+                            //获取贵人所坐之干
+                            if (mapTianYi.get(listMingGan[j]).contains(luZhi)) {
+                                mapResult["【天禄贵神】"]="如更入格，贵极一品，常格得之，不下三品。（玉霄宝鉴）"
+                            }
+
+                        }
+                    }
+                }
+            }
+
+
+        }
 
         mapResult;
     }
