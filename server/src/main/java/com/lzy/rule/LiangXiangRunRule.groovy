63712d8a9d54325b34a4dec3370119dcbabcf0ec
214234574@qq.com <1026long>
2017-07-19 08:03:13 +0800

    增加梁湘润接口母法

--- server/src/main/java/com/lzy/rule/LiangXiangRunRule.groovy
+++ server/src/main/java/com/lzy/rule/LiangXiangRunRule.groovy
@@ -1,5 +1,6 @@
 package com.lzy.rule
 
+import com.lzy.common.TianGanEnum
 import com.lzy.demo.BaZi
 import com.lzy.demo.CommonAlgorithm
 import com.lzy.rule.BaseRule
@@ -42,12 +43,32 @@ public class LiangXiangRunRule implements BaseRule{
 
         def luZhi = commonAlgorithm.getLuZhi(nianGan)
 
+        def renZhi = commonAlgorithm.getShiShenDiZhi(riGan, "刃")
+        def yinZhi = commonAlgorithm.getShiShenDiZhi(riGan, "印")
 
         //五行缺一规则
         //子平母法规则
         def mufa=[:]
         mapResult["子平母法规则"]=mufa
-        mufa["shuanglu"]="双鹿"
+
+        //1，羊刃格，劫财透月干，四地支中又见二支为正印禄，既羊刃格带印极旺，凶灾.
+        //按照此方法只有 木和金日主才有 更严格只有 甲和庚才有
+        matchStr=riGan+yueGan+yueZhi
+        def countYin = bazi.getDiZhiShiShenCount("印")
+        def countJiecai = bazi.getDiZhiShiShenCount("劫财")
+        if("甲乙卯"==matchStr || "庚辛酉"==matchStr ){
+            if(countYin>=2){
+                mufa["羊刃格带印极旺-凶灾"]="羊刃格，劫财透月干，四地支中又见二支为正印禄，既羊刃格带印极旺，凶灾"
+            }
+        }
+        else if(renZhi==yueZhi){
+            //羊刃格包括阴干
+            if(countJiecai>=1&&countYin>=2){
+                mufa["羊刃格带印极旺-凶灾-规则放宽"]="羊刃格，劫财透干，又见二支为正印禄，凶灾，无救"
+            }
+
+
+        }
 
 
         return mapResult
