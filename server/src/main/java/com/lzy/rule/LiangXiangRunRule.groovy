65700d2e5b8a85b230795507811bd4297f73944e
214234574@qq.com <1026long>
2017-07-19 21:02:34 +0800

    增加梁湘润接口母法

--- server/src/main/java/com/lzy/rule/LiangXiangRunRule.groovy
+++ server/src/main/java/com/lzy/rule/LiangXiangRunRule.groovy
@@ -49,18 +49,21 @@ public class LiangXiangRunRule implements BaseRule{
         def listMingZhi = bazi.getListMingZhi()
         def listMingZhu = bazi.getListMingZhu()
 
-        def luZhi = commonAlgorithm.getLuZhi(nianGan)
+        def luZhi = commonAlgorithm.getLuZhi(riGan)
 
         def renZhi = commonAlgorithm.getShiShenDiZhi(riGan, "刃")
         def yinZhi = commonAlgorithm.getShiShenDiZhi(riGan, "印")
+        def jiecai = commonAlgorithm.getTianGanShiShen(riGan, "劫财")
+        def bijian = commonAlgorithm.getTianGanShiShen(riGan, "比肩")
 
         //1，羊刃格，劫财透月干，四地支中又见二支为正印禄，既羊刃格带印极旺，凶灾.
         //按照此方法只有 木和金日主才有 更严格只有 甲和庚才有
         matchStr=riGan+yueGan+yueZhi
         def countYin = bazi.getDiZhiShiShenCount("印")
         def countPianYin = bazi.getDiZhiShiShenCount("偏印")
-        def countJiecai = bazi.getTianGanCount(commonAlgorithm.getTianGanShiShen(riGan,"劫财"))
-        def countBiJian = bazi.getTianGanCount(commonAlgorithm.getTianGanShiShen(riGan,"比肩"))
+
+        def countJiecai = bazi.getTianGanCount(jiecai)
+        def countBiJian = bazi.getTianGanCount(bijian)
 
         if("甲乙卯"==matchStr || "庚辛酉"==matchStr ){
             if(countYin>=2){
@@ -72,7 +75,7 @@ public class LiangXiangRunRule implements BaseRule{
              *  核心概念就是 阳刃格局 带比印 太旺
              * [2017-07-19 add by longzhiyou]
              */
-            //羊刃格包括阴干,印包括正印偏印
+            //羊刃格包括阴干,印包括正印偏印∂
             if((countJiecai+countBiJian)>0&& (countYin+countPianYin)>1){
                 mapResult["羊刃格带印极旺-凶灾-规则放宽"]="羊刃格，劫财透干，又见二支为正印禄，凶灾"
             }
@@ -80,6 +83,24 @@ public class LiangXiangRunRule implements BaseRule{
 
         }
 
+        //2，旺财遇劫带旺印，官非，刑名，意外灾害.
+        def caizhi = commonAlgorithm.getShiShenDiZhi(riGan, "正财")
+        def piancaizhi = commonAlgorithm.getShiShenDiZhi(riGan, "偏财")
+        if((caizhi==yueZhi||piancaizhi==yueZhi) && jiecai==yueGan && (countYin+countPianYin)>1){
+
+            mapResult["旺财遇劫带旺印-官非，刑名，意外灾害"]="月支正财禄，月干劫财，地支有两个印临官位"
+        }
+
+        //3，双禄带比印，孤克之命.
+        if(luZhi==riZhi && bazi.getDiZhiCount(luZhi)>1 && (countYin+countPianYin)>0){
+            if(countJiecai>0){
+                mapResult["双禄带劫财印，大凶之命"]="日主坐禄，又重见禄支，天干透劫财，更有[印]专旺之支"
+            }else if(countBiJian>0 ){
+                mapResult["双禄带比印，孤克之命"]="日主坐禄，又重见禄支，天干透比，更有[印]专旺之支"
+            }
+
+
+        }
 
         return mapResult
     }
