5d18befc7f809e257a895bca3f5c1781a6ee1155
longzhiyou <long.zhiyou@bestskip.com>
2017-09-04 15:59:34 +0800

    更新

---
+++ server/src/main/java/com/lzy/rule/SuiYunRule.groovy
@@ -0,0 +1,21 @@
+package com.lzy.rule
+
+import com.lzy.core.BaZi
+import com.lzy.core.CommonAlgorithm
+
+/**
+ * 岁运规则
+ *  子平母法规则
+ * Created by bukeyan on 2017/6/18.
+ */ class SuiYunRule implements BaseRule{
+
+    @Override
+    Object matchRule(BaZi bazi, CommonAlgorithm commonAlgorithm) {
+
+        def mapResult = [:]
+
+        return mapResult
+    }
+
+
+}
