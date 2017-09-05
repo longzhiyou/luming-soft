29fd0814cb46afd5dc569c1f61bac576719106a5
longzhiyou <long.zhiyou@bestskip.com>
2017-09-05 16:40:54 +0800

    更新十干

--- server/src/main/java/com/lzy/controller/GroovyController.groovy
+++ server/src/main/java/com/lzy/controller/GroovyController.groovy
@@ -6,6 +6,7 @@ import com.lzy.domain.AnalyzeResult
 import com.lzy.repository.MatchRuleRepository
 import com.lzy.rule.CoreRule
 import com.lzy.rule.LiangXiangRunRule
+import com.lzy.rule.ReferenceRule
 import com.lzy.rule.ShenShaRule
 import com.lzy.rule.ShiZhanRule
 import com.lzy.rule.WomanRule
@@ -30,13 +31,14 @@ class GroovyController {
 
     final static def listRule = [new WuXingJingJiRule()]
     final static def mapRule = [
-
+            "参考":new ReferenceRule(),
             "核心":new CoreRule(),
             "神煞":new ShenShaRule(),
             "实务":new ShiZhanRule(),
             "女命":new WomanRule(),
             "五行精纪":new WuXingJingJiRule(),
             "梁湘润":new LiangXiangRunRule()
+
     ]
 
     @RequestMapping(method= RequestMethod.GET)
