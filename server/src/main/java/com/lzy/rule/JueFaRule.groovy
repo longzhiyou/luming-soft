5ec60961a9e0edb38fa62ee28645aef41f9a07c4
longzhiyou <long.zhiyou@bestskip.com>
2017-09-20 09:23:18 +0800

    调整

---
+++ server/src/main/java/com/lzy/rule/JueFaRule.groovy
@@ -0,0 +1,16 @@
+package com.lzy.rule
+
+import com.lzy.core.BaZi
+import com.lzy.core.CommonAlgorithm
+
+/**
+ *  诀法规则规则
+ * Created by bukeyan on 2017/6/18.
+ */
+class JueFaRule implements BaseRule{
+
+   @Override
+    def matchRule(BaZi bazi, CommonAlgorithm commonAlgorithm) {
+       return null
+   }
+}
