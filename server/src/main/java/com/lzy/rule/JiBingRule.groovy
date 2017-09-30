d1268ab6a436b8034daca5a78daef319e3bffd4a
longzhiyou <long.zhiyou@bestskip.com>
2017-09-30 11:56:21 +0800

    更新

---
+++ server/src/main/java/com/lzy/rule/JiBingRule.groovy
@@ -0,0 +1,16 @@
+package com.lzy.rule
+
+import com.lzy.core.BaZi
+import com.lzy.core.CommonAlgorithm
+
+/**
+ *  疾病规则
+ * Created by bukeyan on 2017/6/18.
+ */
+class JiBingRule implements BaseRule{
+
+   @Override
+    def matchRule(BaZi bazi, CommonAlgorithm commonAlgorithm) {
+       return null
+   }
+}
